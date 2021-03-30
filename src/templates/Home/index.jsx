29c523b91087715cd907e1b27d0d2642191a4471
Tiago Thompson <rigothompsontiago@gmail.com>
2021-03-30 15:18:11 -0300

    Ajustes.

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,126 +1,82 @@
-import './style.css';
-import { Component } from 'react';
-import { loadPosts } from '../../utils/load-posts';
+import { useCallback, useEffect, useState } from 'react';
+
+import './styles.css';
+
 import { Posts } from '../../components/Posts';
+import { loadPosts } from '../../utils/load-posts'
 import { Button } from '../../components/Button';
-import { InputSearch } from '../../components/InputSearch';
+import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 2,
-    searchValue: ''
-  };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(2);
+  const [searchValue, setSearchValue] = useState('');
 
-  // Semelhante ao $scope.on no AngularJS
-  // É invocado imediatamente após um componente ser montado (inserido na árvore).
-  // Inicializações que exijam nós do DOM devem vir aqui.
-  // Se precisar carregar data de um endpoint remoto, este é um bom lugar para instanciar sua requisição.
-  async componentDidMount(){
-    await this.loadPosts();
-  }
-  
-  // Chamando postagens/imagens
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
-
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    });
-  }
+    
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-  // Carrega mais posts / Paginação
-  loadMorePosts = () => {
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
+  useEffect(() => {
+    console.log(new Date().toLocaleString('pt-BR'));
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
 
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   }
 
-  // Função para o search
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-
-    this.setState({ searchValue: value })
+    setSearchValue(value);
   }
 
-  // é invocado imediatamente após alguma atualização ocorrer.
-  // Este método não é chamado pelo initial render.
-  componentDidUpdate(){
-    // 
-  }
-  
-  // é invocado imediatamente antes que um componente seja desmontado e destruído.
-  // Qualquer limpeza necessária deve ser realizada neste método, como invalidar timers,
-  // cancelar requisições de rede, ou limpar qualquer subscrição que foi criada no componentDidMount()
-  componentWillUnmount(){
-    // 
-  }
-
-  // Ao renderizar no navegador
-  // Observação: Quando o estado é atualizado, este render será chamado novamente
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-    const filteredPosts = !!searchValue ? 
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filteredPosts = !!searchValue ?
     allPosts.filter(post => {
       return post.title.toLowerCase().includes(
-        searchValue.toLocaleLowerCase()
-        );
+        searchValue.toLowerCase()
+      );
     })
     : posts;
 
-    return (
-      <section className="container">
-
+  return (
+    <section className="container">
       <div className="search-container">
-        { !!searchValue && (
+        {!!searchValue && (
           <h1>Search value: {searchValue}</h1>
         )}
-        
-        {/* Exibe quantidade de posts encontrados */}
-        { !!searchValue && (
-          <span>Findeds posts: {filteredPosts.length}</span>
+
+        <TextInput searchValue={searchValue} handleChange={handleChange} />
+      </div>
+
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length === 0 && (
+        <p>Não existem posts =(</p>
+      )}
+
+      <div className="button-container">
+        {!searchValue && (
+          <Button
+            text="Load more posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts}
+          />
         )}
+      </div>
+    </section>
+  );
 
-        {/* Campo de pesquisa */}
-        <InputSearch searchValue={searchValue} handleChange={this.handleChange} />
-
-        </div>
-
-        {/* Posts */}
-        {/* Se existir posts exibe tudo */}
-        { filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
-        )}
-
-        {/* Se não existir posts exibe texto */}
-        { filteredPosts.length === 0 && (
-          <p>Ops... Não achamos o que você procurou :)</p>
-        )}
-
-        {/* Botão */}
-        <div className="button-container">
-          { !searchValue && (
-            <Button
-              text="Load more posts"
-              onClick={this.loadMorePosts}
-              disabled={noMorePosts}
-            />
-          )}
-        </div>
-      </section>
-    );
-  }
 }
